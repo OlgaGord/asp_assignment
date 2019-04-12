b168badec7fa7ba1b8a7c88b249e69c568431408
unknown <olga1gordeyeva@gmail.com>
2019-04-12 09:04:47 -0400

    asp.net mvc v1.1

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+/*
+
+        // GET: /Home/Details/5 
+
+        public ActionResult Details(int id)
+
+        {
+
+            return View();
+
+        }
+
+        //
+
+        // GET: /Home/Create 
+
+        public ActionResult Create()
+
+        {
+
+            return View();
+
+        }
+
+        //
+
+        // POST: /Home/Create 
+
+        [AcceptVerbs(HttpVerbs.Post)]
+
+        public ActionResult Create(FormCollection collection)
+
+        {
+
+            try
+
+            {
+
+                // TODO: Add insert logic here 
+
+                return RedirectToAction("Index");
+
+            }
+
+            catch
+
+            {
+
+                return View();
+
+            }
+
+        }
+
+        //
+
+        // GET: /Home/Edit/5
+
+        public ActionResult Edit(int id)
+
+        {
+
+            return View();
+
+        }
+
+        //
+
+        // POST: /Home/Edit/5 
+
+        [AcceptVerbs(HttpVerbs.Post)]
+
+        public ActionResult Edit(int id, FormCollection collection)
+
+        {
+
+            try
+
+            {
+
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+
+            }
+
+            catch
+
+            {
+
+                return View();
+
+            }
+
+        }
+*/
+
+
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "At Luxe Diamond Jewelers the passion for beautiful jewelry and friendly service is obvious..";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Please Visit Luxe Diamond Jewelers at the following location.";
+
+            return View();
+        }
+
+
+
+
+
+    }
+}
